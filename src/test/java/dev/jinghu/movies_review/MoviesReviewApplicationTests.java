30f3cd593efed4b282af18ca1925ee404cb28998
Jing <jhu8187@gmail.com>
2023-07-01 16:42:58 -0400

    Initial commit

---
+++ src/test/java/dev/jinghu/movies_review/MoviesReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jinghu.movies_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
